f54fb5f1fe20deb8bb5f85f2c4ad1b942bf1605e
Pilly Perez <ignacia.pp@gmail.com>
2018-11-03 04:37:27 -0300

    add exercise 11

--- learnyounode/exercise-11.js
+++ learnyounode/exercise-11.js
@@ -1,1 +1,18 @@
-exercise-11.js
+const http = require('http');
+const fs = require('fs');
+
+const [language, programName, port, pathfile] = process.argv;
+
+const server = http.createServer((req, res) => {
+	const readStream = fs.createReadStream(pathfile);
+	readStream.on('open', () => {
+		readStream.pipe(res);
+	});
+
+	readStream.on('error', (err) => {
+		res.end(err);
+	});
+
+});
+
+server.listen(port);
