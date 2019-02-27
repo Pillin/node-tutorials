5918e7f910d0cda8c9ebea3d23f7df8869116d29
Pillippa Perez <pillippa@23people.io>
2019-02-26 23:45:01 -0300

    Add stupid solution for stupid exercise 7 =/

---
+++ stream-adventure/exercise-7.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const through = require('through2');
+
+const [nodeProgram, fileName, port] = process.argv;
+
+const server = http.createServer((req, res) => {
+	const { method, pipe} = req;
+
+	function write (buf, _, next) {
+      this.push(buf.toString().toUpperCase());
+      next();
+    }
+
+    req.pipe(through(write)).pipe(res);
+});
+
+server.listen(port);
