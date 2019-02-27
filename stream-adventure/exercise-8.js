432eceb1428d6b0caa7035712b55765af6a7b273
Pillippa Perez <pillippa@23people.io>
2019-02-27 00:09:29 -0300

    add exercise number 8 stream adventure

---
+++ stream-adventure/exercise-8.js
@@ -0,0 +1,10 @@
+const URL = 'http://localhost';
+const PORT = '8099';
+const request = require('request');
+
+
+const { argv, stdin, stdout } = process;
+
+const req =  request.post(`${URL}:${PORT}`);
+
+stdin.pipe(req).pipe(stdout);
