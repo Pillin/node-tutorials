ef16da2f91d7b53032d4cb0fb9efc5913a5baa17
Pillippa Perez <pillippa@23people.io>
2019-03-28 23:13:22 -0300

    add exercise 4 functional javascript

--- functional-javascript-workshop/exercise-4.js
+++ functional-javascript-workshop/exercise-4.js
@@ -1,6 +1,8 @@
 const PHRASE_MAX_LENGTH = 50;
 
 const getShortMessages = messages =>
-  messages.filter(elem => elem.length > PHRASE_MAX_LENGTH);
+  messages
+    .filter(elem => elem.message.length < PHRASE_MAX_LENGTH)
+    .map(elem => elem.message);
 
 module.exports = getShortMessages;
